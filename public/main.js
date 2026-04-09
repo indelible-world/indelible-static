70d1a357684be30468cbc9a9c4861da8168a31f4
Will Patti <wdppostcard@gmail.com>
2026-04-08 21:53:49 -0400

    add basic nav

---
+++ public/main.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.topnav a').forEach(link => {
+  link.addEventListener('click', function () {
+    document.querySelector('.topnav a.active')?.classList.remove('active');
+    this.classList.add('active');
+  });
+});
+
+if (!location.hash) {
+    location.hash = '#attestations';
+}
