3664c98eae1507ebbf214017b5ea66af29e33e49
Will Patti <wdppostcard@gmail.com>
2026-04-22 22:40:57 -0400

    fix vite build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   base: '/indelible-static/',
-}
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        publish: resolve(__dirname, 'publish.html'),
+      },
+    },
+  },
+})
