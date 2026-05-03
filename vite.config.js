5cda3716b4db0c8b07ede4d5fbc2b49b56c9fcf4
Will Patti <wdppostcard@gmail.com>
2026-05-03 14:03:24 -0400

    allow for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/indelible-static/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
